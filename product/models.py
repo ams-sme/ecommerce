6eee8ccebe489ccbfa4d1651861abe7aad2c8a63
rabetbishwokarma <adreeyen@gmail.com>
2023-06-27 21:44:32 -0400

    Class

--- product/models.py
+++ product/models.py
@@ -4,6 +4,9 @@ class Product():
 
 
 class Sales: 
-    def __init__(self, product_id)
+    def __init__(self, product_id, product_name ):
         self.product_id = product_id
-        
+        self.product_name = product_name
+
+
+
