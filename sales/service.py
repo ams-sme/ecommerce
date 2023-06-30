4717eadf5509e4ed88ab17b6e255a5ed91435364
Shiva itani <shivaitani@Shivas-MacBook-Pro.local>
2023-06-29 20:17:45 -0400

    new class updated

---
+++ sales/service.py
@@ -0,0 +1,14 @@
+class Transaction:
+    def __init__(self) -> None:
+        pass
+        
+class Detail: 
+    def __init__(self) -> None:
+        pass
+        
+class Dispute: 
+    def __init__(self) -> None:
+        
+        
+               
+    
